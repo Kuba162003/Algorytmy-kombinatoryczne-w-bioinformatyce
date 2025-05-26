891008c5b252244cd3d564ce976779c25296a646
Jakub Pędziński <126468460+Kuba162003@users.noreply.github.com>
2025-05-26 22:55:15 +0200

    Add files via upload

---
+++ Generator instancji mapowanie/Generator instancji mapowanie/Generator instancji mapowanie.cpp
@@ -0,0 +1,59 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include<fstream>
+#include<vector>
+#include<algorithm>
+#include <iostream>
+#include<string>
+#include<sstream>
+#include<random>
+using namespace std;
+void elements_sums(int idx, int sum, vector<int>& map_of_regions, vector<int>& vector_of_sums) {
+    if (idx >= map_of_regions.size())
+        return;
+    sum += map_of_regions[idx++];
+    vector_of_sums.push_back(sum);
+    elements_sums(idx, sum, map_of_regions, vector_of_sums);
+}
+vector<int> add_sums(vector<int>& map_of_regions) {
+    vector<int>vector_of_sums;
+    for (int i = 0; i < map_of_regions.size(); i++) {
+        elements_sums(i, 0, map_of_regions, vector_of_sums);
+    }
+
+    return vector_of_sums;
+}
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    int tmp;
+    string line;
+    vector<int>map_of_regions;
+    getline(cin, line);
+    line = line;
+    stringstream stream(line);
+    while (stream >> tmp) {
+        map_of_regions.push_back(tmp);
+    }
+    vector<int>vector_of_sums = add_sums(map_of_regions);
+    sort(vector_of_sums.begin(), vector_of_sums.end());
+    //reverse(vector_of_sums.begin(), vector_of_sums.end());
+    //shuffle(vector_of_sums.begin(), vector_of_sums.end(), gen);
+
+    for (auto& x : vector_of_sums) {
+        cout << x << ' ';
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ Mapowanie/Mapowanie/Mapowanie.cpp
@@ -0,0 +1,192 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+vector<int> Load();
+int Silnia(int n);
+int Cuts(int A);
+int First_element();
+void Search(int index, int max_index);
+bool In_set(int number);
+bool Correct();
+int Check_used();
+vector<int> set;
+vector<int> map;
+vector<bool> used;
+bool end_program = false;
+
+void Wyswietl_zbior(vector<int> zbior);
+void Wyswietl_uzyte();
+
+int main() {
+	set = Load();
+	sort(set.begin(), set.end());
+	int length = set.back();
+	int cuts = Cuts(set.size());
+	if (cuts == -1) {
+		cout << "Nie można skonstruować mapy";
+		return 0;
+	}
+	for (int i = 0; i < set.size(); i++) {
+		used.push_back(false);
+	}
+	int first = First_element();
+	map.push_back(first);
+	clock_t t = clock();
+	Search(first, cuts + 1);
+	if (end_program == false) {
+		cout << "Nie znaleziono rozwiazania";
+		return 0;
+	}
+
+	t = clock() - t;
+	double time_of_algorithm = t;
+	time_of_algorithm = time_of_algorithm / CLOCKS_PER_SEC;
+	cout << endl << "Czas: " << time_of_algorithm;
+
+	return 1;
+}
+
+vector<int> Load() {
+	fstream plik;
+	plik.open("instancja11b.txt", ios::in);
+	if (plik.good() == false) {
+		cout << "Nie ma pliku";
+		exit(0);
+	}
+
+	vector<int> instance;
+	int fragment;
+	while (plik >> fragment) {
+		instance.push_back(fragment);
+	}
+
+	plik.close();
+	return instance;
+}
+
+int Silnia(int n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	else {
+		return n * Silnia(n - 1);
+	}
+}
+
+int Cuts(int A) {
+	A = 8 * A;
+	A = A + 1;
+	A = sqrt(A);
+	A = A - 3;
+	A = A / 2;
+	return A;
+}
+
+int First_element() {
+	int first = set[set.size() - 1] - set[set.size() - 2];
+	for (int i = 0; i < set.size(); i++) {
+		if (set[i] == first) {
+			used[i] = true;
+			break;
+		}
+	}
+	return first;
+}
+
+void Search(int index, int max_index) {
+	if (map.size() == max_index) {
+		if (Check_used() == 0) {
+			return;
+		}
+		cout << "Wynik: ";
+		for (int i = 0; i < map.size(); i++) {
+			cout << map[i] << ", ";
+		}
+		end_program = true;
+		return;
+	}
+	vector<bool> start_used;
+	for (int i = 0; i < set.size(); i++) {
+		if (used[i] == true)
+			continue;
+		map.push_back(set[i]);
+		vector<bool> tmp_used = used;
+		if (Correct()) {
+			used[i] = true;
+			Wyswietl_zbior(map);
+			Search(set[i], max_index);
+			if (end_program)
+				return;
+			used = tmp_used;
+		}
+		else {
+			used = tmp_used;
+			map.pop_back();
+		}
+	}
+	used = start_used;
+	map.pop_back();
+	return;
+}
+
+bool In_set(int number) {
+	for (int i = 0; i < set.size(); i++) {
+		if (used[i] == true) {
+			continue;
+		}
+		if (set[i] == number) {
+			used[i] = true;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Correct() {
+	vector<bool> in;
+	for (int i = 0; i < map.size() - 1; i++) {
+		in.push_back(false);
+	}
+	int tmp = map[map.size() - 1];
+	for (int i = map.size() - 2; i >= 0; i--) {
+		if (In_set(tmp + map[i])) {
+			in[i] = true;
+			tmp = tmp + map[i];
+		}
+	}
+
+	for (int i = 0; i < in.size(); i++) {
+		if (in[i] == false) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+void Wyswietl_zbior(vector<int> zbior) {
+	for (int i = 0; i < zbior.size(); i++) {
+		cout << zbior[i] << ", ";
+	}
+	cout << endl;
+}
+
+void Wyswietl_uzyte() {
+	for (int i = 0; i < used.size(); i++) {
+		cout << i << ": " << used[i] << endl;
+	}
+	cout << endl;
+}
+
+int Check_used() {
+	for (int i = 0; i < used.size(); i++) {
+		if (used[i] == false) {
+			return 0;
+		}
+	}
+	return 1;
+}
